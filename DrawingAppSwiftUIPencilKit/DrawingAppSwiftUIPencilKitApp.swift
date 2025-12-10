483f2085b815adcce0536d2151b5f136fb2e8e16
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2025-12-10 16:02:53 +0400

    Initial Commit

---
+++ DrawingAppSwiftUIPencilKit/DrawingAppSwiftUIPencilKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAppSwiftUIPencilKitApp.swift
+//  DrawingAppSwiftUIPencilKit
+//
+//  Created by Hakob Ghlijyan on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAppSwiftUIPencilKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
